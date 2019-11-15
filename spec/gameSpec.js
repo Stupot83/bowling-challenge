f48e6cd811f3681de50a68fffbbd65d692d14335
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-11-15 14:55:07 +0000

    :construction::white_check_mark: Add logic to set number of frames being played and load finalframe at tenth frame with all tests passing

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -31,8 +31,32 @@
         expect(typeof game.numberOfFramesPlayed).toEqual("function");
       });
 
+      it("responds to setupGame", () => {
+        expect(typeof game.setupGame).toEqual("function");
+      });
+
       it("starts with number of frames as 0", () => {
         expect(game.numberOfFramesPlayed()).toEqual(0);
+      });
+
+      describe("Tracking the number of frames when game has started", () => {
+
+        beforeEach(() => {
+          game.setupGame(Frame);
+        });
+
+        it('should not allow more than 10 frames per game', () => {
+          expect(() => {
+            game.setupGame(Frame);
+          }).toThrow(
+            new Error("Ten frames played, please start a new game")
+          );
+        });
+
+        it('should create a total of 10 frames per game', () => {
+          expect(game.frames.length).toEqual(10);
+          expect(game.numberOfFramesPlayed()).toEqual(10);
+        });
       });
     });
   });
--- src/game.js
+++ src/game.js
@@ -6,4 +6,16 @@ class Game {
   numberOfFramesPlayed() {
     return this.frames.length;
   }
+
+  setupGame(element) {
+    if (this.numberOfFramesPlayed() === 10) {
+      throw new Error("Ten frames played, please start a new game");
+    }
+
+    for (var i = 0; i < 9; i++) {
+      this.frames.push(new element);
+    }
+
+    this.frames.push(new FinalFrame());
+  }
 }
