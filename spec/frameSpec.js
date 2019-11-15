b25dd9b8d6a3f2f45c32f2a00c1660b6e3985d69
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-11-15 10:34:48 +0000

    :construction::white_check_mark: Add code to handle the number of turns each frame including an error if trying to have more than 2 turns with all tests passing

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,10 +1,10 @@
-(function () {
+(function() {
   "use strict";
 
   describe("Frame", () => {
     let frame;
 
-    beforeEach(() => { 
+    beforeEach(() => {
       frame = new Frame();
     });
 
@@ -21,8 +21,16 @@
         expect(typeof frame.totalPins).toEqual("number");
       });
 
-      it('should always start with ten pins', () => {
+      it("responds to playerTurn", () => {
+        expect(typeof frame.numberOfTurns).toEqual("number");
+      });
+
+      it("should always start with ten pins", () => {
         expect(frame.totalPins).toEqual(10);
+      });
+
+      it("should always start with numberOfTurns at 0", () => {
+        expect(frame.numberOfTurns).toEqual(0);
       });
     });
 
@@ -36,6 +44,29 @@
         expect(frame.totalPins).toEqual(4);
       });
     });
-  });
 
+    describe("Frame should only have two turns per player", () => {
+
+      it("numberOfTurns updates", () => {
+        frame.playerTurn(6);
+        expect(frame.numberOfTurns).toEqual(1);
+      });
+
+      it("numberOfTurns updates after two turns", () => {
+        frame.playerTurn(6);
+        frame.playerTurn(3);
+        expect(frame.numberOfTurns).toEqual(2);
+      });
+
+      it("players cannot have more than two turns", () => {
+        frame.playerTurn(6);
+        frame.playerTurn(3);
+        expect(() => {
+          frame.playerTurn(6);
+        }).toThrow(
+          new Error("Three turns is cheating!")
+        );
+      });
+    });
+  });
 })();
--- src/frame.js
+++ src/frame.js
@@ -1,9 +1,15 @@
 class Frame {
   constructor() {
     this.totalPins = 10;
+    this.numberOfTurns = 0;
   }
 
   playerTurn(pinsdown) {
+    if (this.numberOfTurns === 2) {
+      throw new Error("Three turns is cheating!");
+    }
+    
     this.totalPins -= pinsdown;
+    this.numberOfTurns++;
   }
 }
