f80da5650ef4cbebd5670da1e4ec538c0fc730e4
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-11-15 10:21:11 +0000

    :construction::white_check_mark: Add additional test coverage for initial frame setup

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,7 +9,6 @@
     });
 
     describe("Frame initialisation", () => {
-
       it("instantiates the Frame class successfully", () => {
         expect(frame).toBeInstanceOf(Frame);
       });
@@ -18,8 +17,23 @@
         expect(typeof frame).toEqual("object");
       });
 
+      it("has an attribute of totalPins ", () => {
+        expect(typeof frame.totalPins).toEqual("number");
+      });
+
       it('should always start with ten pins', () => {
         expect(frame.totalPins).toEqual(10);
+      });
+    });
+
+    describe("Frame can update number of pins left", () => {
+      it("responds to playerTurn", () => {
+        expect(typeof frame.playerTurn).toEqual("function");
+      });
+
+      it("players turn updates totalpins", () => {
+        frame.playerTurn(6);
+        expect(frame.totalPins).toEqual(4);
       });
     });
   });
--- src/frame.js
+++ src/frame.js
@@ -2,4 +2,8 @@ class Frame {
   constructor() {
     this.totalPins = 10;
   }
+
+  playerTurn(pinsdown) {
+    this.totalPins -= pinsdown;
+  }
 }
